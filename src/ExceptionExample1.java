9c538078f7e4b50d28ae19565d54898c55e0dcd8
Setthanuwat <aftnick2@gmail.com>
2023-01-11 15:08:17 +0700

    Initial commit

---
+++ src/ExceptionExample1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ExceptionExample1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        System.out.println("You have entered: "+number);
+
+    }
+}
+
---
+++ src/StackOverflow.java
@@ -0,0 +1,9 @@
+class StackOverflow {
+    public static void test(int i) {
+        if (i == 0)
+            return;
+        else
+            test(i++);
+    }
+}
+
